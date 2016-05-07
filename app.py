fb441a545e4cf0a2ce5f9268475ca8219055fa33
VicCebedo <cebedo.vii@gmail.com>
2016-05-07 21:43:27 +0800

    Project groundwork and basics.

--- app.py
+++ app.py
@@ -5,17 +5,23 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 
+RESPONSE_INVALID_2FA = "Invalid 2FA."
+
+
+# Get the secret of the user from db.
+# Check if the token is valid.
+def token_valid(user_name, totp_token):
+    totp_secret = dao_user.get_user_secret(user_name)
+    token_valid = otp.valid_totp(totp_token, totp_secret)
+    return token_valid
+
 
 # Get ID of entries.
 @app.route('/user/<user_name>/2fa/<totp_token>/message/', methods=['GET'])
 def get_all_messages(user_name, totp_token):
-    # Get the secret of the user from db.
-    # Check if the token is valid.
-    totp_secret = dao_user.get_user_secret(user_name)
-    token_valid = otp.valid_totp(totp_token, totp_secret)
-    if not token_valid:
+    if not token_valid(user_name, totp_token):
         # TODO Redirect to error code.
-        return "Invalid 2FA."
+        return RESPONSE_INVALID_2FA
 
     # Fetch the ID's.
     message_ids = dao_message.get_message_ids_by_receiver(user_name)
@@ -30,27 +36,39 @@ def get_all_messages(user_name, totp_token):
 
 
 # Get entry.
-@app.route('/user/<user_name>/message/<message_id>', methods=['GET'])
-def get_message(user_name, message_id):
+@app.route('/user/<user_name>/2fa/<totp_token>/message/<message_id>', methods=['GET'])
+def get_message(user_name, totp_token, message_id):
+    if not token_valid(user_name, totp_token):
+        # TODO Redirect to error code.
+        return RESPONSE_INVALID_2FA
     message = dao_message.get_message(message_id)
     return str(message)
 
 
 # Add entry.
-@app.route('/message/', methods=['POST'])
-def post_message():
+@app.route('/user/<user_name>/2fa/<totp_token>/message/', methods=['POST'])
+def post_message(user_name, totp_token):
+    if not token_valid(user_name, totp_token):
+        # TODO Redirect to error code.
+        return RESPONSE_INVALID_2FA
     return 'post_message'
 
 
 # Delete entry.
-@app.route('/user/<user_name>/message/<message_id>', methods=['DELETE'])
-def delete_message(user_name, message_id):
+@app.route('/user/<user_name>/2fa/<totp_token>/message/<message_id>', methods=['DELETE'])
+def delete_message(user_name, totp_token, message_id):
+    if not token_valid(user_name, totp_token):
+        # TODO Redirect to error code.
+        return RESPONSE_INVALID_2FA
     return 'delete_message' + user_name + message_id
 
 
 # Delete all entries.
-@app.route('/user/<user_name>/message/', methods=['DELETE'])
-def delete_all_messages(user_name):
+@app.route('/user/<user_name>/2fa/<totp_token>/message/', methods=['DELETE'])
+def delete_all_messages(user_name, totp_token):
+    if not token_valid(user_name, totp_token):
+        # TODO Redirect to error code.
+        return RESPONSE_INVALID_2FA
     return 'delete_all_messages' + user_name
 
 
--- app_dao/dao_user.py
+++ app_dao/dao_user.py
@@ -6,6 +6,10 @@ MONGO_PORT = 27017
 MONGO_DB = "nomsg"
 MONGO_COLLECTION = "users"
 
+COLUMN_ID = "_id"
+COLUMN_USER_NAME = "user_name"
+COLUMN_TOTP_SECRET = "totp_secret"
+
 
 # Add a new entry.
 def create_user(user_name):
@@ -17,7 +21,7 @@ def create_user(user_name):
     # Do operation.
     if user_name is not None:
         totp_secret = pyotp.random_base32()
-        user = {"user_name": user_name, "totp_secret": totp_secret}
+        user = {COLUMN_USER_NAME: user_name, COLUMN_TOTP_SECRET: totp_secret}
         users.insert_one(user).inserted_id
     client.close()
 
@@ -30,11 +34,11 @@ def get_user_secret(user_name):
     users = db[MONGO_COLLECTION]
 
     # Get the user's secret.
-    totp_secret = users.find_one({"user_name": user_name}, {"totp_secret": 1, "_id": 0})
+    totp_secret = users.find_one({COLUMN_USER_NAME: user_name}, {COLUMN_TOTP_SECRET: 1, COLUMN_ID: 0})
 
     # Return blank if null.
     # Otherwise, normal behaviour.
     if totp_secret is None:
         return ""
-    user_secret = totp_secret['totp_secret']
+    user_secret = totp_secret[COLUMN_TOTP_SECRET]
     return user_secret
