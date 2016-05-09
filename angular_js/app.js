084f9170c1bdd886a4635c7cd6e34ef6afc21beb
VicCebedo <cebedo.vii@gmail.com>
2016-05-09 22:40:44 +0800

    Finalized core functionalities before adding CSS framework.

--- angular_js/app.js
+++ angular_js/app.js
@@ -5,11 +5,10 @@ angular.module('NoMsg', [
   'ngRoute',
   'NoMsg.home',
   'NoMsg.messagepost',
-  'NoMsg.messagepostanon',
   'NoMsg.messagegetall',
   'NoMsg.signup'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider
-  .otherwise({redirectTo: '/'});
+  .otherwise({redirectTo: '/home'});
 }]);
