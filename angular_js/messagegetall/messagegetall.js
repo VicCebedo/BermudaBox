64284175bb1631b9dbca174cced97f3c168fee3e
VicCebedo <cebedo.vii@gmail.com>
2016-05-10 14:31:09 +0800

    Added functionality to delete all messages.

--- angular_js/messagegetall/messagegetall.js
+++ angular_js/messagegetall/messagegetall.js
@@ -5,10 +5,25 @@ angular.module('NoMsg.messagegetall', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/messagegetall', {
     templateUrl: 'messagegetall/messagegetall.html',
-    controller: 'MessageGetAllCtrl'
+    controller: 'InboxCtrl'
   });
 }])
-.controller('MessageGetAllCtrl', function($scope, $http){
+.controller('InboxCtrl', function($scope, $http){
+
+    // If the delete button is pressed.
+    $scope.deleteAll = function() {
+        var user2fa = $scope.input2fa;
+        var username = $scope.inputUsername;
+
+        // @app.route('/user/<user_name>/2fa/<totp_token>/message/', methods=['DELETE'])
+        var url = "http://127.0.0.1:5000/user/"+username+"/2fa/"+user2fa+"/message/";
+        $http.delete(url)
+        .then(function(response){
+            var responseData = response.data;
+            $scope.responseText = responseData;
+            $scope.responseData = "";
+        });
+    };
 
     // If the submit button is pressed.
     $scope.submit = function() {
